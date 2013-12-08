efc834d45968af3c3d64912a76a444058d1a2c6d
dsugiyama <dsugiyama@outlook.jp>
2013-12-08 23:26:35 +0900

    initial build

---
+++ winmain.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace irr::core;
+using namespace irr::gui;
+using namespace irr::io;
+using namespace irr::scene;
+using namespace irr::video;
+
+INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PWSTR pCmdLine, INT nCmdShow)
+{
+	return 0;
+}
